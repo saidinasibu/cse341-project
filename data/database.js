1ffa6e3928ee45489efc1f4782791bb83d3d1bd8
David Nasibu <adjinasibusaidi@gmail.com>
2025-11-17 23:31:17 -0800

    updated

--- data/database.js
+++ data/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGO_DB_URL);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error);
